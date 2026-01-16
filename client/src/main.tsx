6ef1fe4427ed686bb94d699bc24f5be64aedb64d
Purple Port Setup <setup@purpleport.app>
2026-01-16 16:16:59 +0530

    Add version check log to main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+console.log("🚀 Qix App Version: Task Mgmt Update 1.1");
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <App />
